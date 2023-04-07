2f15de4b969cdd4b5ee07a14619a0543b4274cee
egor <2109383+egornomic@users.noreply.github.com>
2023-04-08 00:44:08 +0200

    check for negative number added in `/esteem`

--- commands/esteem.js
+++ commands/esteem.js
@@ -21,8 +21,26 @@ module.exports = {
     const senderId = interaction.user.id;
     const targetId = interaction.options.getUser('to').id;
     const amount = interaction.options.getNumber('amount');
-    const { guild } = interaction;
 
+    if (amount <= 0) {
+      const cheaterRole = interaction.guild.roles.cache.find(role => role.name === 'Cheater');
+      if (cheaterRole) {
+        const member = await interaction.guild.members.fetch(interaction.user.id);
+        await member.roles.add(cheaterRole);
+        await interaction.reply({
+          content: `Uh-oh! It looks like someone's been caught red-handed! 🚨 You've been branded with the <@&${cheaterRole.id}> role for trying to pull a sneaky one with negative Esteem. Better luck next time, slick!`,
+          ephemeral: true
+        });
+      } else {
+        await interaction.reply({
+          content: `Oh, you sneaky little rascal! 😏 You tried to pull a fast one with negative reputation points, huh? Lucky for you, there's no 'Cheater' role on this server... yet! But remember, I've got my eye on you!`,
+          ephemeral: true
+        });
+      }
+      return;
+    }
+
+    const { guild } = interaction;
     const transferAmount = amount * 0.9;
     const burnAmount = amount * 0.1;
 
