faca5bd84094532f0fd646cae3ff4ff61613cb36
egor <2109383+egornomic@users.noreply.github.com>
2023-04-08 18:32:56 +0200

    added rewards for vc activity

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,21 @@
+const config = require('../config.json');
+const { updateEsteem } = require('../utils/firebase');
+
+const userVoiceTime = new Map();
+
+module.exports = async (client, oldState, newState) => {
+  const userId = newState.id;
+  const guildId = newState.guild.id;
+
+  if (!oldState.channelId && newState.channelId) {
+    // User joined a voice channel
+    userVoiceTime.set(userId, Date.now());
+  } else if (oldState.channelId && !newState.channelId) {
+    // User left a voice channel
+    const startTime = userVoiceTime.get(userId) || Date.now();
+    const timeSpentInVoice = Date.now() - startTime;
+    const repToAdd = Math.floor(timeSpentInVoice / 600000) * config.repConstants.voice;
+    userVoiceTime.delete(userId);
+    await updateEsteem(guildId, userId, repToAdd);
+  }
+};
